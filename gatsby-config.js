ef0acdfb59b01b0ee12c2c088ccadf4141d2d667
Jiri Holusa <jiri@hazelcast.com>
2022-01-09 23:54:34 +0100

    Change site name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Holbrew`,
+    title: `HolBrew`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@jholusa`,
     siteUrl: `https://www.holbrew.cz`,
